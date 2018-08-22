34f9850d9a530c87591ece79e6031d7aabda5d74
Ronen Amiel <ronen.amiel@gmail.com>
2018-08-22 10:06:41 +0300

    revert some changes to generators

--- packages/create-yoshi-app/templates/fullstack/src/client.js
+++ packages/create-yoshi-app/templates/fullstack/src/client.js
@@ -1,3 +1,4 @@
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
--- packages/create-yoshi-app/templates/fullstack/src/server.js
+++ packages/create-yoshi-app/templates/fullstack/src/server.js
@@ -1,34 +1,39 @@
+import 'regenerator-runtime/runtime';
+import wixRunMode from 'wix-run-mode';
 import ejs from 'ejs';
 import wixExpressCsrf from 'wix-express-csrf';
 import wixExpressRequireHttps from 'wix-express-require-https';
+import { readFileSync } from 'fs';
 
 module.exports = (app, context) => {
   const config = context.config.load('{%projectName%}');
+  const templatePath = './src/index.ejs';
+  const templateFile = readFileSync(templatePath, 'utf8');
+  const isProduction = wixRunMode.isProduction();
 
   app.use(wixExpressCsrf());
   app.use(wixExpressRequireHttps);
 
-  app.get('/', async (req, res) => {
-    const {
-      language,
-      basename,
-      debug = process.env.NODE_ENV === 'development',
-    } = req.aspects['web-context'];
+  app.get('/', (req, res) => {
+    const renderModel = getRenderModel(req);
+    const html = ejs.render(templateFile, renderModel, {
+      cache: isProduction,
+      filename: templatePath,
+    });
+    res.send(html);
+  });
 
-    const renderModel = {
-      language,
-      basename,
-      debug,
+  function getRenderModel(req) {
+    return {
+      locale: req.aspects['web-context'].language,
+      basename: req.aspects['web-context'].basename,
+      debug:
+        req.aspects['web-context'].debug ||
+        process.env.NODE_ENV === 'development',
       clientTopology: config.clientTopology,
       title: 'Wix Full Stack Project Boilerplate',
     };
-
-    const html = await ejs.renderFile('./src/index.ejs', renderModel, {
-      cache: !debug,
-    });
-
-    res.send(html);
-  });
+  }
 
   return app;
 };
